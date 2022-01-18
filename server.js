0549a4e259c6bb9b3a6ac4fbd0fa2b823c4b395c
joebobbio <profjstark2@gmail.com>
2022-01-17 19:16:20 -0500

    update

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-const Corrosion = require('corrosion');
-
-const ssl = {
-    key: fs.readFileSync(path.join(__dirname, '/ssl/ssl.key')),
-    cert: fs.readFileSync(path.join(__dirname, '/ssl/ssl.cert')),
-};
-const server = https.createServer(ssl);
-const proxy = new Corrosion({
-    codec: 'xor',
-    prefix: '/get/',
-    cookie: 'true',
-    ws: 'true',
-    title: 'index.html'
-});
-
-proxy.bundleScripts();
-
-server.on('request', (request, response) => {
-    if (request.url.startsWith(proxy.prefix)) return proxy.request(request, response);
-    response.end(fs.readFileSync(__dirname + '/public/index.html', 'utf-8'));
-}).on('upgrade', (clientRequest, clientSocket, clientHead) => proxy.upgrade(clientRequest, clientSocket, clientHead)).listen(8080);
+const
+    https = require('https'),
+    fs = require('fs'),
+    config = require('./config.json'),
+    Corrosion = require('corrosion'),
+    express = require('express'),
+    proxy = new Corrosion({
+        prefix: config.prefix,
+        codec: config.codec || 'xor',
+        ws: true,
+        cookie: true,
+        title: 'index.html',
+        standardMiddleware: true
+    })
+    const app = express()
+    const theproxy = (req, res) => {
+        if (req.url.startsWith(config.prefix)) return proxy.request(req, res);
+        req.pathname = req.url.split('#')[0].split('?')[0];
+        req.query = {};};
+app.use('/', express.static(__dirname + '/public'));
+app.use('/', theproxy);
+https.createServer({
+	  key: fs.readFileSync('ssl/ssl.key'),
+	cert: fs.readFileSync('ssl/ssl.cert')}, app)
+	.listen(process.env.PORT || config.port, function () {console.log(`prxy is running at http://localhost:${config.port}`)});
